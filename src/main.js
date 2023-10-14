638879f9d3167dcbeb70bb6df1b98b022772f514
Shin Ohno <ohnoshin@gmail.com>
2023-10-15 00:40:14 +0900

    Add slack integration

--- src/main.js
+++ src/main.js
@@ -1,14 +1,35 @@
-function test() {
-    var prop = PropertiesService.getScriptProperties().getProperties();
+/**
+ * Retrieves events from a Google Calendar and posts them to a Slack channel.
+ */
+function main() {
+    Logger.log('Gas-Calendar-Slack Started!')
 
-    Logger.log('Hello GitHub Actions Deploy!');
+    const prop = PropertiesService.getScriptProperties().getProperties();
+    const slackApp = SlackApp.create(prop.SLACK_TOKEN)
+    const slackChannelId = prop.SLACK_CHANNEL_ID
+    const calendarId = prop.CALENDAR_ID
+    const tagNotified = 'notified'
 
-    let slackApp = SlackApp.create( prop.SLACK_TOKEN);
-    let slackChannelId = prop.SLACK_CHANNEL_ID;
-    Logger.log(slackChannelId);
+    const now = new Date()
+    const events = CalendarApp.getCalendarById(calendarId).getEventsForDay(now)
+    for (let i = 0; i < events.length; i++) {
+        let event = events[i]
 
-    let myCalendar = CalendarApp.getCalendarById(prop.CALENDAR_ID )
-    let now = new Date();
-    let myEvent = myCalendar.getEventsForDay(now);
-    Logger.log(myEvent[0].getTitle());
+        if (event.getTag(tagNotified)){
+            continue
+        }
+
+        if (event.getStartTime().getTime() - 1 * 60 * 1000 > now) {
+             continue
+        }
+
+        let description = event.getDescription()
+            .replace(/<br>/g, '\n')
+            .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'')
+        let message = `[${event.getTitle()}] \n${description}`
+        Logger.log(message)
+
+        slackApp.postMessage(slackChannelId, message)
+        event.setTag(tagNotified, 'true')
+    }
 }
