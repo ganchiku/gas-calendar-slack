2ede778d6470b6e2ca1a982e42ca96e5d5b5baf6
Shin Ohno <ohnoshin@gmail.com>
2023-10-15 00:24:29 +0900

    Debug for Property for GAS

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 function test() {
+    var prop = PropertiesService.getScriptProperties().getProperties();
+
     Logger.log('Hello GitHub Actions Deploy!');
+
+    let slackApp = SlackApp.create( prop.SLACK_TOKEN);
+    let slackChannelId = prop.SLCK_CHANNEL_ID;
+    Logger.Log(slackChannelId);
+
+    let myCalendar = CalendarApp.getCalendarById(prop.CALENDAR_ID )
+    let now = new Date();
+    let myEvent = myCalendar.getEventsForDay(now);
+    Logger.log(myEvent[0].getTitle());
 }
